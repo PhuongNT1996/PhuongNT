e92c9f089961e1aa3616c3baa79c394705e02d20
PhuongNT1996 <thephuongcqt@gmail.com>
2017-03-02 00:11:08 +0700

    Loaded Products

--- OnlineShopping/OnlineShopping/Controllers/HomeController.cs
+++ OnlineShopping/OnlineShopping/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Model.DAL;
+using Model.Models;
+using Model.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,7 +16,72 @@ namespace OnlineShopping.Controllers
         {
             ViewBag.TitlePage = "Online Shopping | Home";
             ViewBag.Page = "Home";
-            return View();
+
+            HomeProductsModel homeProduct = new HomeProductsModel();
+            ProductDAL productDal = new ProductDAL();
+
+            List<Product> products = productDal.getTopNewProduct();           
+            homeProduct.NewProducts = products;
+
+            products = productDal.getTopTrending();
+            homeProduct.TrendingProducts = products;
+
+            products = productDal.getTopBestSeller();
+            homeProduct.BestSellerProducts = products;
+
+            CatalogueDAL catalogueDal = new CatalogueDAL();
+            List<Catalogue> catalogues = catalogueDal.getAllCatalogues();
+            homeProduct.Catalogues = catalogues;
+
+            return View(homeProduct);
+        }
+
+        public ActionResult NewProduct()
+        {
+            ViewBag.TitlePage = "Online Shopping | Home";
+            ViewBag.Page = "Home";
+
+            HomeProductsModel homeProduct = new HomeProductsModel();
+            ProductDAL productDal = new ProductDAL();
+
+            List<Product> products = productDal.getAllNewProduct();
+
+            return View(products);
+        }
+
+        public ActionResult BestSeller()
+        {
+            ViewBag.TitlePage = "Online Shopping | Home";
+            ViewBag.Page = "Home";
+
+            HomeProductsModel homeProduct = new HomeProductsModel();
+            ProductDAL productDal = new ProductDAL();
+
+            List<Product> products = productDal.getAllBestSeller();
+
+            return View(products);
+        }
+
+        public ActionResult Trending()
+        {
+            ViewBag.TitlePage = "Online Shopping | Home";
+            ViewBag.Page = "Home";
+
+            HomeProductsModel homeProduct = new HomeProductsModel();
+            ProductDAL productDal = new ProductDAL();
+
+            List<Product> products = productDal.getAllTrending();
+
+            return View(products);
+        }
+
+        public ActionResult Catalogue(int ID)
+        {
+            CatalogueDAL catalogueDal = new CatalogueDAL();
+            Catalogue catalogue = catalogueDal.getCatalogueById(ID);
+
+            catalogueDal.getAllProductsByCatalogue(catalogue);
+            return View(catalogue);
         }
     }
 }
